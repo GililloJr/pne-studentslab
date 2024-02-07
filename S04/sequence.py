6d88995f44a136f85b559bf9e2c1fdfb3ff6ebd9
GililloJr <a.gilb.2023@alumnos.urjc.es>
2024-02-07 18:41:16 +0100

    This is going to be my first but not last commit

--- S04/sequence.py
+++ S04/sequence.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 FILENAME = "sequences/ADA.txt"
 file_contents = Path(FILENAME).read_text()
-
 index = file_contents.find('\n')
 file_contents = (file_contents[index:]).replace('\n', '')
 print(len(file_contents))
