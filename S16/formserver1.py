bf57ef5fea13a470a1d4bcfa5ac31a752be40d95
GililloJr <alvargb05@gmail.com>
2024-04-14 18:47:45 +0200

    gg

--- S16/formserver1.py
+++ S16/formserver1.py
@@ -1,13 +1,19 @@
 import http.server
 import socketserver
 import termcolor
-
+from pathlib import Path
+from urllib.parse import parse_qs, urlparse
+import jinja2 as j
 # Define the Server's port
 PORT = 8080
 
+
 # -- This is for preventing the error: "Port already in use"
 socketserver.TCPServer.allow_reuse_address = True
-
+def read_html_file(filename):
+    contents = Path("html/" + filename).read_text()
+    contents = j.Template(contents)
+    return contents
 
 # Class with our Handler. It is a called derived from BaseHTTPRequestHandler
 # It means that our class inherits all his methods and properties
@@ -16,36 +22,33 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         """This method is called whenever the client invokes the GET method
         in the HTTP protocol request"""
-
+        url_path = urlparse(self.path)
+        path = url_path.path  # we get it from here
+        arguments = parse_qs(url_path.query)
+        print(path, arguments)
         # Print the request line
         termcolor.cprint(self.requestline, 'green')
 
-        # IN this simple server version:
-        # We are NOT processing the client's request
-        # It is a happy server: It always returns a message saying
-        # that everything is ok
-
-        # Message to send back to the client
-        url = self.requestline.split(" ")[1]
-        if url == "/":
-            contents = "Welcome to my server"
+        if path == "/":
+            contents = Path("./html/form-1.html").read_text()
+        elif path == "/echo":
+            contents = read_html_file("form-e1.html").render(context={"todisplay": arguments["msg"][0]})
         else:
-            contents = "Resource not available"
+            contents = Path('html/error.html').read_text()
 
 
         # Generating the response message
         self.send_response(200)  # -- Status line: OK!
 
         # Define the content-type header:
-        self.send_header('Content-Type', 'text/plain')
-        self.send_header('Content-Length', len(contents.encode()))
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
 
         # The header is finished
         self.end_headers()
 
         # Send the response message
-        self.wfile.write(contents.encode())
-
+        self.wfile.write(str.encode(contents))
         return
 
 
