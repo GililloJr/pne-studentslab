f4e5c38f13aca6d058f64295f60d6a2cdb0a0af2
GililloJr <alvargb05@gmail.com>
2024-05-04 14:32:39 +0200

    gg

---
+++ Final-Project/server2.py
@@ -0,0 +1,39 @@
+import http.server
+import http.client
+import socketserver
+import termcolor
+from pathlib import Path
+from urllib.parse import parse_qs, urlparse
+import jinja2 as j
+import json
+
+
+PORT = 8080
+SERVER = 'rest.ensembl.org'
+ENDPOINT = '/info/ping'
+PARAMS = '?content-type=application/json'
+URL = SERVER + ENDPOINT + PARAMS
+
+print()
+print(f"Server: {SERVER}")
+print(f"URL: {URL}")
+
+print(f"\nConnecting to server: {SERVER}:{PORT}\n")
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER, PORT)
+
+# -- Send the request message, using the GET method. We are
+# -- requesting the main page (/)
+try:
+    conn.request("GET", ENDPOINT + PARAMS)
+except ConnectionRefusedError:
+    print("ERROR! Cannot connect to the Server")
+    exit()
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+def read_html_file(filename):
+    contents = Path("html/" + filename).read_text()
+    contents = j.Template(contents)
+    return contents
