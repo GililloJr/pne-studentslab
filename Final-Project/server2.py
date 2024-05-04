b090f879430ed1f4e2e791b7589c9d2c764ce033
GililloJr <alvargb05@gmail.com>
2024-05-04 20:32:23 +0200

    gg

--- Final-Project/server2.py
+++ Final-Project/server2.py
@@ -1,55 +1,55 @@
 import http.server
-import http.client
 import socketserver
 import termcolor
-from pathlib import Path
 from urllib.parse import parse_qs, urlparse
 import jinja2 as j
-import json
+import requests
+from pathlib import Path
 
 PORT = 8080
+socketserver.TCPServer.allow_reuse_address = True
+
 def read_html_file(filename):
     contents = Path("html/" + filename).read_text()
     contents = j.Template(contents)
     return contents
-def list_species(limit=None):
-    person = request.get("http://rest.ensembl.org/info/species", context={"Content-Type": "application/json"})
-    if person.ok:
-        datas = person.json()
-        specie = [specie['display_name'] for specie in datas['species']]
-        num_of_species = len(specie)
+
+def get_species_data(limit=None):
+    response = requests.get("http://rest.ensembl.org/info/species", headers={"Content-Type": "application/json"})
+    if response.ok:
+        data = response.json()
+        species = [species['display_name'] for species in data['species']]
         if limit:
-            specie = specie[:limit]
-        return num_of_species, specie
+            species = species[:int(limit)]
+        return len(species), species
     else:
         print("Error!")
 
-def info_of_karyotype(specie):
-    person = requests.get(f"https://rest.ensembl.org/info/assembly/{specie}", context={"Content-Type": "application/json"})
-    if person.ok:
-        datas = person.json
-        return datas['karyotype']
-    else:
-        print("Error")
-
 class TestHandler(http.server.BaseHTTPRequestHandler):
-def do_GET(self):
-    """This method is called whenever the client invokes the GET method
-    in the HTTP protocol request"""
-    url_path = urlparse(self.path)
-    path = url_path.path  # we get it from here
-    arguments = parse_qs(url_path.query)
-    print(path, arguments)
-    # Print the request line
-    termcolor.cprint(self.requestline, 'green')
+    def do_GET(self):
+        url_path = urlparse(self.path)
+        path = url_path.path
+        arguments = parse_qs(url_path.query)
+        print(path, arguments)
+        termcolor.cprint(self.requestline, 'green')
 
-    if path == "/":
-        filename = "index.html"
-        contents = read_html_file(filename).render(context={})
-    elif path == "/listSpecies":
-        filename = "species.html"
-        if 'limit' in arguments:
-            limit = arguments['limit'][-1]
-            num_of_species, specie = list_species(limit)
-        else:
-            limit = None
+        if path == "/":
+            contents = read_html_file("index.html").render()
+        elif path == "/listSpecies":
+            limit = arguments['limit'][-1] if 'limit' in arguments else None
+            num_species, species = get_species_data(limit)
+            contents = read_html_file("species.html").render(num_species=num_species, species=species, limit=limit)
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+        self.end_headers()
+        self.wfile.write(str.encode(contents))
+
+with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
+    print("Serving at PORT", PORT)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Stopped by the user")
+        httpd.server_close()
