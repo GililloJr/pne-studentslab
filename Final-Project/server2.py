9509764d75f27607692c394886a6f14d88308638
GililloJr <alvargb05@gmail.com>
2024-05-05 17:19:20 +0200

    gg

--- Final-Project/server2.py
+++ Final-Project/server2.py
@@ -15,15 +15,44 @@ def read_html_file(filename):
     return contents
 
 def get_species_data(limit=None):
-    response = requests.get("http://rest.ensembl.org/info/species", headers={"Content-Type": "application/json"})
+    url = "http://rest.ensembl.org/info/species"
+    headers = {"Content-Type": "application/json"}
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print("Error!")
+        return
+    data = response.json()
+    species_list = [species['display_name'] for species in data['species']]
+
+    if limit is not None:
+        species_list = species_list[:int(limit)]
+    return len(species_list), species_list
+
+def get_karyotype(species):
+    person = requests.get(f"https://rest.ensembl.org/info/assembly/{species}", headers={"Content-Type": "application/json"})
+    if person.ok:
+        data = person.json()
+        return data['karyotype']
+    else:
+        print("ERROR!")
+
+def get_chromosome_length(species, chromo):
+    response = requests.get(f"https://rest.ensembl.org/info/assembly/{species}",  headers={"Content-Type": "application/json"})
     if response.ok:
         data = response.json()
-        species = [species['display_name'] for species in data['species']]
-        if limit:
-            species = species[:int(limit)]
-        return len(species), species
+        print(data)
+        chromo_length = []
+        for chromosome in data['top_level_region']:
+            if chromosome['name'] == chromo:
+                chromo_length.append(chromosome['length'])
+        if chromo_length:
+            return chromo_length[0]
+        else:
+            return None
+
     else:
-        print("Error!")
+        print("ERROR!")
+
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -39,6 +68,17 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             limit = arguments['limit'][-1] if 'limit' in arguments else None
             num_species, species = get_species_data(limit)
             contents = read_html_file("species.html").render(num_species=num_species, species=species, limit=limit)
+        elif path == "/karyotype":
+            species = arguments['species'][-1] if 'species' in arguments else None
+            karyotype = get_karyotype(species)
+            contents = read_html_file("karyotype.html").render(karyotype=karyotype)
+        elif path == "/chromosomeLength":
+            species = arguments['species'][0] if 'species' in arguments else None
+            chromo = arguments['chromosome'][0] if 'chromo' in arguments else None
+            chromo_length = get_chromosome_length(species, chromo)
+            contents = read_html_file("chromolength.html").render(species=species, chromo=chromo, chromo_length=chromo_length)
+        else:
+            contents = Path('html/error.html').read_text()
 
         self.send_response(200)
         self.send_header('Content-Type', 'text/html')
